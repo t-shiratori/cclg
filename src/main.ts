1c574e7f90693566475dca974cda15a2ebf42eca
Takashi Shiratori <tks.srtr89@gmail.com>
2023-04-16 02:52:44 +0900

    update: sample

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,14 @@
 import { cclg } from './lib/cclg'
 
 cclg({ label: 'any label' })
-cclg({ label: 'any label', color: '#ff1493' })
-cclg({ label: 'any label', color: '#ffffff', bgColor: '#39c093' })
-cclg({ label: 'any label', color: '#ffffff', bgColor: '#39c093', value: '123456789' })
-cclg({ label: 'any label', cStyle: 'color: #ffffff; background: #ff8c00; padding: 4px 12px;' })
-
-cclg({ label: 'any label', type: 'log' })
-cclg({ label: 'any label', type: 'info' })
-cclg({ label: 'any label', type: 'warn' })
-cclg({ label: 'any label', type: 'error' })
+cclg({ label: 'color', color: '#ff1493' })
+cclg({ label: 'color bgColor', color: '#ffffff', bgColor: '#39c093' })
+cclg({ label: 'color bgColor value', color: '#ffffff', bgColor: '#39c093', value: '123456789' })
+cclg({ label: 'cStyle', cStyle: 'color: #ffffff; background: #ff8c00; padding: 4px 12px;' })
+cclg({ label: 'type log', type: 'log' })
+cclg({ label: 'type info', type: 'info' })
+cclg({ label: 'type warn', type: 'warn' })
+cclg({ label: 'type error', type: 'error' })
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
