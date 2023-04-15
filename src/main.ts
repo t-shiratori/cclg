d32a62b2e1748f4de781a145dd22ed43aa1ba326
Takashi Shiratori <tks.srtr89@gmail.com>
2023-04-16 00:59:09 +0900

    delete files

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,3 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter'
-
 import { cclg } from './lib/main'
 
 cclg({ label: 'any label' })
@@ -16,22 +11,9 @@ cclg({ label: 'any label', type: 'info' })
 cclg({ label: 'any label', type: 'warn' })
 cclg({ label: 'any label', type: 'error' })
 
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
+    hello
   </div>
 `
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
